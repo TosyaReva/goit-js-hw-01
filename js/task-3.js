cd0ea581dea9883f5d3020dfa8cb3ba5a11f818e
TosyaReva <tosyan93@gmail.com>
2024-10-13 00:32:09 +0300

    done tasks

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function getElementWidth(content, padding, border) {
+    const contentNumber = Number.parseFloat(content)
+
+    const paddingNumber = Number.parseFloat(padding)
+
+    const borderNumber = Number.parseFloat(border)
+
+    const result = contentNumber + paddingNumber + paddingNumber + borderNumber + borderNumber
+    return result
+}
+
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
